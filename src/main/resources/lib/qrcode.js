2392a4e9d4a65125d2c8395e8b39145c9aa947b6
Alex Rodriguez <aro@enonic.com>
2018-11-02 13:11:49 +0100

    Preparing lib for release

--- src/main/resources/lib/qrcode.js
+++ src/main/resources/lib/qrcode.js
@@ -2,8 +2,5 @@ exports.generateQrCode = function (params) {
     var bean = __.newBean('com.enonic.lib.qrcode.QRCodeHandler');
     bean.text = __.nullOrValue(params.text) || '';
     bean.size = __.nullOrValue(params.size) || 250;
-
-    var result = bean.generateQrCode();
-
-    return result;
+    return bean.generateQrCode();
 };
